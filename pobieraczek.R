751a271d2bf50d8d5c1f55c5ebe3de427c14e994
MartynaSpiewak <spiewak.martyna@gmail.com>
2015-04-21 15:07:09 +0200

    pobieraczek : update

--- pobieraczek.R
+++ pobieraczek.R
@@ -3,22 +3,38 @@
 # poprawilam na te potrzeby fnkcje merge.csv() (tzn. zdazyl to zrobic Piotrek lub Martyna
 # ale jeszcze dopisalam Wam tam "/" :P)
 
-folder_z_moja_czescia <- "test_linki"
-file_destination <- "rekordy"
-
-pobieraczek <- funkcja( file_destination, folder_z_moja_czescia ){
+pobieraczek <- function(from, to=from){
+  
+    if(!file.exists(file.path("dane"))){
+      # jesli nie, tworzymy takowy
+      dir.create(file.path("dane"))
+    }
+    if(!file.exists(file.path("dane/glowne"))){
+      # jesli nie, tworzymy takowy
+      dir.create(file.path("dane/glowne"))
+    }
+    #lista plikow w folderze
+    which_to_download <- unlist(sapply(as.character(from:to), 
+            function(x) list.files(paste0(getwd(),"/movies_link"), 
+                        x, full.names=TRUE)), 
+                              use.names = FALSE)
+    if(length(which_to_download)==0){
+      return(invisible(NULL))
+    }
+    #interesujace nas pliki
+    #files<-files[which_to_download]
+    names_of_files<-stri_sub(which_to_download,-8,-5)
+    names_of_table<-paste0("dane/glowne/",names_of_files,".csv")
+    
+    for(j in 1:length(names_of_table)){
       
-      plik_linkow <- merge_csv( folder_z_moja_czescia )
-      dest_file <- paste( destination,"csv",sep="." )
-      if( dest_file%in%list.files(getwd()) )
-            message("Dopisuje dane do istniejacego pliku!")
-      
-      con <- file(dest_file)
-      liczba_linkow <- nrow(plik_linkow)
+      con <- file(names_of_table[j])
+      plik_linkow <- read.table(which_to_download[j],header=TRUE)$link
+      liczba_linkow <- length(plik_linkow)
       i=1
       open(con,"a")
       while( i <= liczba_linkow ){
-            link <- as.character(plik_linkow[i,2])
+            link <- as.character(plik_linkow[i])
             a <- from_main_page(link)
             b <- from_full_cast_n_crew(link)
             c <- from_page_source(link)
@@ -26,15 +42,16 @@ pobieraczek <- funkcja( file_destination, folder_z_moja_czescia ){
             suppressWarnings(
                   write.table(
                         as.data.frame(c(a,b,c,d)),
-                        file=dest_file, append=TRUE, sep=";", row.names=FALSE,
-                        col.names=!( file.exists(dest_file) )
+                        file=names_of_table[j], append=TRUE, sep=";", row.names=FALSE,
+                        col.names=!( file.exists(names_of_table[j]) )
                   )
             )
             i=i+1
       }
       close(con)
+    }
       cat("\nDone\n")
 }
 
-pobieraczek( destination, plik_linkow )
+pobieraczek(1894)
 
