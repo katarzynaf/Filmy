09311a27b8cbf1ba655d9bb9cfe49dcb4ba9e39d
katarzynaf <fk_katarzyna@gmail.com>
2015-04-21 15:23:02 +0200

    commit

--- pobieraczek.R
+++ pobieraczek.R
@@ -1,7 +1,6 @@
-# widze to jakos tak, ze jak sie dzielimy rokami, to wrzucamy sobie csv-ki z naszymi latami do 
-# jednego folderu i on z niego wszystko automatycznie laczy i pobiera :)
-# poprawilam na te potrzeby fnkcje merge.csv() (tzn. zdazyl to zrobic Piotrek lub Martyna
-# ale jeszcze dopisalam Wam tam "/" :P)
+# widze to jakos tak, ze jak sie dzielimy rokami, to wrzucamy sobie csv-ki z naszymi latami do jednego folderu i on z niego wszystko
+# automatycznie laczy i pobiera :) poprawilam na te potrzeby fnkcje merge.csv() (tzn. zdazyl to zrobic Piotrek lub Martyna ale jeszcze dopisalam
+# Wam tam '/' :P)
 library(XML)
 library(rvest)
 library(dplyr)
@@ -11,35 +10,29 @@ library(stringi)
 folder_z_moja_czescia <- "test_linki"
 file_destination <- "rekordy"
 
-pobieraczek <- function( file_destination, folder_z_moja_czescia ){
-      
-      plik_linkow <- merge_csv( folder_z_moja_czescia )
-      dest_file <- paste( file_destination,"csv",sep="." )
-      if( dest_file%in%list.files(getwd()) )
-            message("Dopisuje dane do istniejacego pliku!")
-      
-      con <- file(dest_file)
-      liczba_linkow <- nrow(plik_linkow)
-      i=1
-      open(con,"a")
-      while( i <= liczba_linkow ){
-            link <- as.character(plik_linkow[i,2])
-            a <- from_main_page(link)
-            b <- from_full_cast_n_crew(link)
-            c <- from_page_source(link)
-            d <- keywords(link)
-            suppressWarnings(
-                  write.table(
-                        as.data.frame(c(a,b,c,d)),
-                        file=dest_file, append=TRUE, sep=";", row.names=FALSE,
-                        col.names=!( file.exists(dest_file) )
-                  )
-            )
-            i=i+1
-      }
-      close(con)
-      cat("\nDone\n")
+pobieraczek <- function(file_destination, folder_z_moja_czescia) {
+    
+    plik_linkow <- merge_csv(folder_z_moja_czescia)
+    dest_file <- paste(file_destination, "csv", sep = ".")
+    if (dest_file %in% list.files(getwd())) 
+        message("Dopisuje dane do istniejacego pliku!")
+    
+    con <- file(dest_file)
+    liczba_linkow <- nrow(plik_linkow)
+    i = 1
+    open(con, "a")
+    while (i <= liczba_linkow) {
+        link <- as.character(plik_linkow[i, 2])
+        a <- from_main_page(link)
+        b <- from_full_cast_n_crew(link)
+        c <- from_page_source(link)
+        d <- keywords(link)
+        suppressWarnings(write.table(as.data.frame(c(a, b, c, d)), file = dest_file, append = TRUE, sep = ";", row.names = FALSE, col.names = !(file.exists(dest_file))))
+        i = i + 1
+    }
+    close(con)
+    cat("\nDone\n")
 }
 
-pobieraczek( "sraniewbanie", "test_linki" )
-
+pobieraczek("sraniewbanie", "test_linki")
+ 
