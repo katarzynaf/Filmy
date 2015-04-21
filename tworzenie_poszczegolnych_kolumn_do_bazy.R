bb742aa02067554415780bbc0347fe17fc388899
katarzynaf <fk_katarzyna@gmail.com>
2015-04-21 14:28:51 +0200

    nie ma.

--- tworzenie_poszczegolnych_kolumn_do_bazy.R
+++
@@ -1,8 +0,0 @@
-library(XML)
-library(rvest)
-library(dplyr)
-library(RCurl)
-library(stringi)
-
-link <- "http://www.imdb.com/title/tt3844454/"
-# funkcja pomocnicza do scala wielu informacji w jeden wektor
